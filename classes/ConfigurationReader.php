b819d239d0f2ad701c1f680d480dd110cc972b9c
Louis Carrese <carrese.louis@gmail.com>
2015-05-18 16:01:10 +0200

    Added ConfigurationReader for use by the plugins front end

---
+++ classes/ConfigurationReader.php
@@ -0,0 +1,49 @@
+<?php 
+namespace LcGenconf;
+
+class ConfigurationReader {
+  private $wpKey;
+  private $values;
+
+  private $currentRepeater;
+
+  public function __construct($wpKey) {
+    $this->wpKey = $wpKey;
+    $this->values = json_decode(get_option($this->wpKey), true);
+
+    if(!isset($this->values[1])) {
+      $this->currentRepeater = 0;
+    }
+  }
+
+  public function setCurrentRepeater($repeaterId) {
+    $this->currentRepater = $repeaterId;
+  }
+
+  public function findRepeater($sectionKey, $fieldKey, $keyValue) {
+    foreach($this->values as $repeaterId => $conf) {
+      if(isset($conf[$sectionKey])
+	 && isset($conf[$sectionKey][$fieldKey])
+	 && $conf[$sectionKey][$fieldKey] == $keyValue) {
+	$this->setCurrentRepeater($repeaterId);
+	return $repeaterId;
+      } else {
+	return false;
+      }
+    }
+  }
+
+  public function get($sectionId, $keyId) {
+    if(isset($this->values[$this->currentRepeater])
+       && isset($this->values[$this->currentRepeater][$sectionId])
+       && isset($this->values[$this->currentRepeater][$sectionId][$keyId])) {
+	 return $this->values[$this->currentRepeater][$sectionId][$keyId];
+       } else {
+	 return null;
+       }
+  }
+  
+
+}
+
+ ?>
